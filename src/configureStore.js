9e93d1e7e17df4013781f44a722e43c50c99deec
raymondma1 <raymondxma@gmail.com>
2018-11-11 16:17:47 -0800

    firebase integration

--- src/configureStore.js
+++ src/configureStore.js
@@ -3,6 +3,30 @@ import thunkMiddleware from 'redux-thunk';
 import monitorReducersEnhancer from './enhancers/monitorReducers';
 import loggerMiddleware from './middleware/logger';
 import rootReducer from './reducers';
+import 'firebase/auth';
+import 'firebase/database';
+import 'firebase/firestore';
+import firebase from 'firebase/app';
+import {reduxFirestore}  from 'redux-firestore';
+import {reactReduxFirebase} from 'react-redux-firebase';
+
+const rrfConfig = {
+  userProfile: 'users',
+  useFirestoreForProfile: true
+}
+
+const firebaseConfig = {
+  // firebaseConfig here
+}
+
+firebase.initializeApp(firebaseConfig);
+
+firebase.firestore().settings({ timestampsInSnapshots: true });
+
+const createStoreWithFirebase = compose(
+  reduxFirestore(firebase),
+  reactReduxFirebase(firebase, rrfConfig)
+)(createStore);
 
 export default function configureStore(preloadedState) {
   const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
@@ -10,7 +34,7 @@ export default function configureStore(preloadedState) {
   const middlewareEnhancer = applyMiddleware(...middlewares)
   const enhancers = [middlewareEnhancer, monitorReducersEnhancer]
   const composedEnhancers = composeEnhancers(...enhancers)
-  const store = createStore(rootReducer, preloadedState, composedEnhancers)
+  const store = createStoreWithFirebase(rootReducer, preloadedState, composedEnhancers)
 
   if (process.env.NODE_ENV !== 'production' && module.hot) {
     module.hot.accept('./reducers', () => store.replaceReducer(rootReducer))
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,13 @@
 
 import { combineReducers } from 'redux'
 import userReducer from './userReducer';
+import { firestoreReducer } from 'redux-firestore';
+import { firebaseReducer } from 'react-redux-firebase';
 
 const rootReducer = combineReducers({
-  userReducer
+  userReducer,
+  firebaseReducer,
+  firestoreReducer
 })
 
 export default rootReducer
